8379df3c34485b4358d27e893ae3b1a120609297
Virginia Wu <virginiawu11@gmail.com>
2021-12-28 22:55:17 -0500

    sumvalues complete

--- ntree.js
+++ ntree.js
@@ -14,7 +14,22 @@ class Tree {
 
   /** sumValues(): add up all of the values in the tree. */
 
-  sumValues() {}
+  sumValues() {
+    const stackToVisit = this.root ? [this.root] : [];
+    let sum = 0;
+    while (stackToVisit.length) {
+      console.log("s", { stackToVisit });
+
+      let current = stackToVisit.pop();
+
+      sum += current.val;
+      console.log({ stackToVisit, current });
+      for (let child of current.children) {
+        stackToVisit.push(child);
+      }
+    }
+    return sum;
+  }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
